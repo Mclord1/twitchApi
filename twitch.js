8c4b2a41ba5a6536802f72f3f2061ad5ad26faf9
Mclord <mclord@initsng.com>
2017-11-07 12:53:17 +0100

    Status of channel updated.

--- twitch.js
+++ twitch.js
@@ -1,76 +1,80 @@
-var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "terakilobyte", "thomasballinger", "beohoff", "MedryBW", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "henk", "dreamhack", "twitch", "bazza87", "lirik"];
-
-function getChannels() {
-    $.each(channels, function (index, channel) {
-        // Create url with parameters needed
+var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "henk", "dreamhack", "twitch", "bazza87", "lirik"];
+    
+function getChannels() {                         
+    $.each(channels, function( index, channel ) {
+    // Create url with parameters needed
         function makeUrl(type, channel) {
             return "https://wind-bow.gomix.me/twitch-api/" + type + "/" + channel + '?callback=?';
         }
-
-        // Toggle status of channel from Api
-        $.getJSON(makeUrl("streams", channel), function (data) {
-            console.log(data.stream);
+ 
+        // Get channel info
+        $.getJSON(makeUrl("streams", channel), function(data) {
+            var game, status;
             if (data.stream === null) {
-                $('.new').addClass('red')
+                game = "Offline";
+                status = "offline";
+            } else if (data.stream === undefined) {
+                game = "Account Closed";
+                status = "offline";
             } else {
-                $('.new').addClass('green');
-            }
-            console.log(data.stream);
-        });
-
-        // Get data from Api
-        $.getJSON(makeUrl("channels", channel), function (data) {
-
-            $('#channels').append(
-                "<a href='" + data.url + "' target='_blank' class='channel'>" +
-                "<div class='left clearfix'><div id='image' class='pull-left'>" +
-                "<img src='" + (data.logo === null ? "https://goo.gl/c5CP66" : data.logo) + " 'id='img' class='rounded-circle' width='70px' height='70px'/>" +
-                "</div>" +
-                "<div id='title' class='clearfix'>" +
-                "<h4>" + data.display_name + "</h4>" +
-                "<p>" + (data.game === null ? "---" : data.game) + "</p>" +
-                "<span class='new'></span>" +
-                "</div>" +
-                "</div><hr></a>"
-            )
+                game = data.stream.game;
+                status = "online";
+            };
+            
+            // Get channel info from Api
+            $.getJSON(makeUrl("channels", channel), function(data) {
+                $('#channels').append (
+                    "<a href='" + data.url + "' target='_blank' class='channel_list " + status + "'>" +
+                        "<div class='left clearfix'><div id='image' class='pull-left'>" +
+                            "<img src='" + (data.logo === null ? "https://goo.gl/c5CP66" : data.logo) + " 'id='img' class='rounded-circle' width='70px' height='70px'/>" +
+                        "</div>" +
+                        "<div id='title' class='clearfix'>" +
+                            "<h4>" + data.display_name + "</h4>" +
+                            "<p>" + (data.game === null ? "___" : data.game) + "</p>" +
+                            "<p class='game'>" + game + "</p>" +
+                        "</div>" +
+                    "</div><hr></a>" 
+                )
+            });
+            
         });
     });
-}
+};
 
-$('document').ready(function () {
+$('document').ready(function() {
     getChannels();
 });
 
-// $('#greenBtn').click(function () {
-//     $('.list>.green').show();
-//     $('.list>.red').hide();
-// });
-//
-// $('#redBtn').click(function () {
-//     $('.list>.red').show();
-//     $('.list>.green').hide();
-// });
-//
-// $('#allBtn').click(function () {
-//     $('.list>.green, .red').show();
-// });
-
-$('#faLeft').click(function () {
-    reload(true);
+$('#greenBtn').click(function() {
+    $('.online').show();
+    $('.offline').hide();
 });
 
-$('#faRight').click(function () {
+$('#redBtn').click(function() {
+    $('.offline').show();
+    $('.online').hide();
+});
+
+$('#allBtn').click(function() {
+    $('.online, .offline').show();
+});
+
+$('#faLeft').click(function() {
+    window.location.href = "";
+});
+
+$('#faRight').click(function() {
     $('.search').toggle(500);
 });
 
-$('#search').keyup(function () {
-    var valThis = $(this).val().toLowerCase();
-    if (valThis === "") {
-        $('.channel').show();
+$('#search').keyup(function(){
+   var valThis = $(this).val().toLowerCase();
+    if(valThis == ""){
+        $('.channel_list').show();           
     } else {
-        $('.channel').each(function () {
+        $('.channel_list').each(function(){
             var text = $(this).text().toLowerCase();
             (text.indexOf(valThis) >= 0) ? $(this).show() : $(this).hide();
         });
-    }
+   };
 });
